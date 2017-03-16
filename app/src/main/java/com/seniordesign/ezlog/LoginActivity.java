e22c0f14fcf014e2a217709dfea7a0a477442cc5
ckcampbell <ckcamp@udel.edu>
2017-03-16 19:05:20 -0400

    added ability to pull username and password from login page

--- app/src/main/java/com/seniordesign/ezlog/LoginActivity.java
+++ app/src/main/java/com/seniordesign/ezlog/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.seniordesign.ezlog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendLogin(View view) {
+        Intent i= new Intent(this, LoggedInMainActivity.class);
+        Bundle login = new Bundle();
+        EditText user = (EditText) findViewById(R.id.loginUsername);
+        EditText pass = (EditText) findViewById(R.id.loginPassword);
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+        login.putString("uname", username);
+        login.putString("pword", password);
+        i.putExtras(login);
+        startActivity(i);
     }
 }
